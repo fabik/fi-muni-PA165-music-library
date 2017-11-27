28f67fb07b8ca1604c034bfc03f50c61ffdebf6a
Korkoran <david.koncak@seznam.cz>
2017-11-27 23:04:34 +0100

    Update SongFacadeImplTest.java

--- music-library-service/src/test/java/cz/fi/muni/pa165/musiclibrary/service/facade/SongFacadeImplTest.java
+++ music-library-service/src/test/java/cz/fi/muni/pa165/musiclibrary/service/facade/SongFacadeImplTest.java
@@ -1,22 +1,16 @@
+
 package cz.fi.muni.pa165.musiclibrary.service.facade;
 
 import cz.fi.muni.pa165.musiclibrary.dto.AlbumDTO;
+import cz.fi.muni.pa165.musiclibrary.dto.GenreDTO;
+import cz.fi.muni.pa165.musiclibrary.dto.MusicianDTO;
 import cz.fi.muni.pa165.musiclibrary.dto.SongDTO;
+import cz.fi.muni.pa165.musiclibrary.entity.Album;
+import cz.fi.muni.pa165.musiclibrary.entity.Genre;
+import cz.fi.muni.pa165.musiclibrary.entity.Musician;
 import cz.fi.muni.pa165.musiclibrary.entity.Song;
 import cz.fi.muni.pa165.musiclibrary.facade.SongFacade;
-import cz.fi.muni.pa165.musiclibrary.service.BeanMappingService;
-import cz.fi.muni.pa165.musiclibrary.service.SongService;
 import cz.fi.muni.pa165.musiclibrary.service.config.ServiceConfiguration;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import org.hibernate.service.spi.ServiceException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -24,43 +18,183 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/**
- *
- * @author David
- */
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.testng.annotations.BeforeMethod;
+
 @ContextConfiguration(classes = ServiceConfiguration.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
 @Transactional
 public class SongFacadeImplTest extends AbstractTestNGSpringContextTests {
-    
+
     @Autowired
     private SongFacade songFacade;
-    
+
     @PersistenceContext
     private EntityManager em;
     
-    private AlbumDTO album;
+    private Song song1;
+    private SongDTO songDTO;
+    private Musician musician;
+    private Genre genre;
     
+    @BeforeMethod
+    public void init(){
+        Album album = createSampleAlbum("Hybrid Theory", "2000-10-24");
+        musician = createSampleMusician("Linkin Park");
+        genre = createSampleGenre("Nu metal");
+
+        songDTO = new SongDTO();
+        songDTO.setAlbum(createAlbumDTO(album));
+        songDTO.setMusician(createMusicianDTO(musician));
+        songDTO.setGenre(createGenreDTO(genre));
+        songDTO.setTitle("In The End");
+        songDTO.setBitrate(320);
+        songDTO.setPosition(0);
+        
+        song1 = new Song();
+        song1.setAlbum(album);
+        song1.setMusician(musician);
+        song1.setGenre(genre);
+        song1.setTitle("In The End");
+        song1.setBitrate(320);
+        song1.setPosition(0);
+    }
+
+
     @Test
     public void testCreate() {
-        album = new AlbumDTO();
-        album.setTitle("album");
-        em.persist(album);
-        SongDTO songDTO = new SongDTO();
-        songDTO.setTitle("Pisnicka");
-        songDTO.setBitrate(120);
-        songDTO.setPosition(2);
-        songDTO.setAlbum(album);
-
         songFacade.create(songDTO);
 
-        List<Song> songs = em.createQuery("SELECT s FROM Song s", Song.class).getResultList();
+        List<Song> songs = getAllSongs();
         Assert.assertEquals(1, songs.size());
-        Assert.assertEquals("Pisnicka", songs.get(0).getTitle());
+        Assert.assertEquals("In The End", songs.get(0).getTitle());
+    }
+    
+    @Test
+    public void testDelete(){
+        
+        songFacade.delete(songDTO);
+        
+        List<Song> songs = getAllSongs();
+        Assert.assertEquals(0, songs.size());
+    }
+    
+    @Test
+    public void testFindById(){
+        em.persist(song1);
+        
+        SongDTO songDto = songFacade.findById(song1.getId());
+        
+        Assert.assertEquals(song1.getId(), songDto.getId());
+        Assert.assertEquals("In The End", songDto.getTitle());
+    }
+    
+    @Test
+    public void testFindAll(){
+        em.persist(song1);
+        
+        List<SongDTO> songDTOs = songFacade.findAll();
+        
+        Assert.assertEquals(1, songDTOs.size());
+        Assert.assertEquals(song1.getTitle(), songDTOs.get(0).getTitle());
+    }
+    
+    @Test
+    public void testFindByTitle(){
+        em.persist(song1);
+        
+        List<SongDTO> songDTOs = songFacade.findByTitle("In The End");
+        
+        Assert.assertEquals(1, songDTOs.size());
+        Assert.assertEquals(song1.getId(), songDTOs.get(0).getId());
+    }
+    
+    @Test
+    public void testFindByMusician(){
+        em.persist(song1);
+        
+        List<SongDTO> songDTOs = songFacade.findByMusician(createMusicianDTO(musician));
+        
+        Assert.assertEquals(1, songDTOs.size());
+        Assert.assertEquals(song1.getTitle(), songDTOs.get(0).getTitle());
+        Assert.assertEquals(song1.getPosition(), songDTOs.get(0).getPosition());
+    }
+    
+    @Test
+    public void testFindByGenre(){
+        em.persist(song1);
+        
+        List<SongDTO> songDTOs = songFacade.findByGenre(createGenreDTO(genre));
+        
+        Assert.assertEquals(1, songDTOs.size());
+        Assert.assertEquals(song1.getTitle(), songDTOs.get(0).getTitle());
+        Assert.assertEquals(song1.getPosition(), songDTOs.get(0).getPosition());
+    }
+
+    private Album createSampleAlbum(String title, String date) {
+        Album album = new Album();
+        album.setReleaseDate(createDate(date));
+        album.setTitle(title);
+        em.persist(album);
+        return album;
+    }
+
+    private AlbumDTO createAlbumDTO(Album album) {
+        AlbumDTO albumDTO = new AlbumDTO();
+        albumDTO.setId(album.getId());
+        albumDTO.setReleaseDate(album.getReleaseDate());
+        albumDTO.setTitle(album.getTitle());
+        albumDTO.setCommentary(album.getCommentary());
+        albumDTO.setAlbumArt(album.getAlbumArt());
+        return albumDTO;
+    }
+
+    private Musician createSampleMusician(String name) {
+        Musician musician = new Musician();
+        musician.setName(name);
+        em.persist(musician);
+        return musician;
+    }
+
+    private MusicianDTO createMusicianDTO(Musician musician) {
+        MusicianDTO musicianDTO = new MusicianDTO();
+        musicianDTO.setId(musician.getId());
+        musicianDTO.setName(musician.getName());
+        return musicianDTO;
+    }
+
+    private Genre createSampleGenre(String name) {
+        Genre genre = new Genre();
+        genre.setName(name);
+        em.persist(genre);
+        return genre;
+    }
+
+    private GenreDTO createGenreDTO(Genre genre) {
+        GenreDTO genreDTO = new GenreDTO();
+        genreDTO.setId(genre.getId());
+        genreDTO.setName(genre.getName());
+        return genreDTO;
+    }
+
+    private Date createDate(String date) {
+        try {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                return dateFormat.parse(date);
+        } catch (ParseException ex) {
+                throw new RuntimeException(ex);
+        }
+    }
+
+    private List<Song> getAllSongs() {
+        return em.createQuery("SELECT s FROM Song s", Song.class).getResultList();
     }
     
 }
